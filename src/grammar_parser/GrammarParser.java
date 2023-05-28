0e37a75e0467cd5f848a2778c584928ac9c29f44
GrunCrow <albamrqz751@gmail.com>
2023-05-28 15:19:43 +0200

    RulesTable created in GrammarParser

--- src/grammar_parser/GrammarParser.java
+++ src/grammar_parser/GrammarParser.java
@@ -1,10 +1,12 @@
 package grammar_parser;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class GrammarParser {
     private Lexer lexer;
     private Token currentToken;
+    private RuleTable ruleTable;
 
     public GrammarParser(String filePath) {
         try {
@@ -13,6 +15,7 @@ public class GrammarParser {
             e.printStackTrace();
         }
         getNextToken();
+        ruleTable = new RuleTable(); // Inicializar con una tabla vacía
     }
 
     private void getNextToken() {
@@ -31,15 +34,18 @@ public class GrammarParser {
     }
 
     private void definicion() {
+    	System.out.println(currentToken.getLexeme());
+    	System.out.println(currentToken.getKind());
         if (currentToken.getKind() == TokenKind.NOTERMINAL) {
+        	String leftHandSide = currentToken.getLexeme();
             match(TokenKind.NOTERMINAL);
             match(TokenKind.EQ);
-            listaReglas();
+            listaReglas(leftHandSide);
             match(TokenKind.SEMICOLON);
         } else if (currentToken.getKind() == TokenKind.COMENTARIO) {
             parseComment();
         } else {
-            reportError("Se esperaba un simbolo no terminal");
+        	match(TokenKind.NOTERMINAL);
         }
     }
 
@@ -48,22 +54,75 @@ public class GrammarParser {
         getNextToken();
     }
 
-    private void listaReglas() {
-        regla();
+    private void listaReglas(String leftHandSide) {
+        regla(leftHandSide);
         while (currentToken.getKind() == TokenKind.BAR) {
             match(TokenKind.BAR);
-            regla();
+            regla(leftHandSide);
         }
     }
 
-    private void regla() {
+    private void regla(String leftHandSide) {
+        String[] rightHandSide = new String[0];
         while (currentToken.getKind() == TokenKind.NOTERMINAL || currentToken.getKind() == TokenKind.TERMINAL) {
-            if (currentToken.getKind() == TokenKind.NOTERMINAL) {
-                match(TokenKind.NOTERMINAL);
+        	String symbol;
+            if (currentToken.getKind() == TokenKind.TERMINAL) {
+                symbol = currentToken.getLexeme();
+                getNextToken();
             } else {
-                match(TokenKind.TERMINAL);
+                symbol = currentToken.getLexeme();
+                getNextToken();
+            }
+            rightHandSide = appendToArray(rightHandSide, symbol);
+        }
+
+        // Obtener el índice de fila y columna en la tabla de reglas
+        int rowIndex = getRuleRowIndex(leftHandSide);
+        int columnIndex = getRuleColumnIndex(rightHandSide);
+
+        // Crear y guardar la regla en la tabla de reglas
+        Rule rule = new Rule(leftHandSide, rightHandSide);
+        ruleTable.setRule(rowIndex, columnIndex, rule);
+    }
+    
+    private String[] appendToArray(String[] array, String element) {
+        int length = array.length;
+        String[] newArray = Arrays.copyOf(array, length + 1);
+        newArray[length] = element;
+        return newArray;
+    }
+    
+    private int getRuleRowIndex(String leftHandSide) {
+        for (int row = 0; row < ruleTable.getRowCount(); row++) {
+            Rule rule = ruleTable.getRule(row, 0);
+            if (rule != null && rule.getLeftHandSide().equals(leftHandSide)) {
+                return row;
             }
         }
+        return ruleTable.getRowCount(); // Si no se encuentra el leftHandSide en la tabla de reglas
+    }
+
+    private int getRuleColumnIndex(String[] rightHandSide) {
+        for (int column = 0; column < ruleTable.getColumnCount(); column++) {
+            Rule rule = ruleTable.getRule(0, column);
+            if (rule != null && rule.getRightHandSide().length == rightHandSide.length) {
+                boolean matches = true;
+                for (int i = 0; i < rightHandSide.length; i++) {
+                    if (!rule.getRightHandSide()[i].equals(rightHandSide[i])) {
+                        matches = false;
+                        break;
+                    }
+                }
+                if (matches) {
+                    return column;
+                }
+            }
+        }
+        return ruleTable.getColumnCount(); // Si no se encuentra el rightHandSide en la tabla de reglas
+    }
+    
+    public RuleTable getRulesTable() {
+    	return ruleTable;
     }
 
     private void match(int expectedTokenKind) {
--- src/grammar_parser/RuleTable.java
+++ src/grammar_parser/RuleTable.java
@@ -2,12 +2,23 @@ package grammar_parser;
 
 public class RuleTable {
     private Rule[][] table;
+    private int rowCount;
+    private int columnCount;
 
+    public RuleTable() {
+        table = new Rule[0][0];
+        rowCount = 0;
+        columnCount = 0;
+    }
+    
     public RuleTable(int rows, int columns) {
         table = new Rule[rows][columns];
+        rowCount = rows;
+        columnCount = columns;
     }
 
     public void setRule(int row, int column, Rule rule) {
+        expandTable(row, column);
         table[row][column] = rule;
     }
 
@@ -16,13 +27,28 @@ public class RuleTable {
     }
 
     public int getRowCount() {
-        return table.length;
+        return rowCount;
     }
 
     public int getColumnCount() {
-        if (table.length == 0) {
-            return 0;
+        return columnCount;
+    }
+
+    private void expandTable(int row, int column) {
+        if (row >= rowCount || column >= columnCount) {
+            int newRowCount = Math.max(row + 1, rowCount);
+            int newColumnCount = Math.max(column + 1, columnCount);
+
+            Rule[][] newTable = new Rule[newRowCount][newColumnCount];
+
+            // Copiar las reglas existentes a la nueva tabla
+            for (int i = 0; i < rowCount; i++) {
+                System.arraycopy(table[i], 0, newTable[i], 0, columnCount);
+            }
+
+            table = newTable;
+            rowCount = newRowCount;
+            columnCount = newColumnCount;
         }
-        return table[0].length;
     }
 }
