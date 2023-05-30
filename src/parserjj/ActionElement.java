2ed6cdfaa66813c86b50e47226f6316166591feb
GrunCrow <albamrqz751@gmail.com>
2023-05-30 13:49:10 +0200

    Todos los ficheros necesarios

---
+++ src/parserjj/ActionElement.java
@@ -0,0 +1,61 @@
+package parserjj;
+
+
+/**
+ * Elemento de la tabla de acci�n
+ */
+public class ActionElement {
+
+	/**
+	 * Constante que identifica un elemento de tipo aceptar
+	 */
+	public static final int ACCEPT = 0;
+	
+	/**
+	 * Constannte que identifica un elemento de tipo shift
+	 */
+	public static final int SHIFT = 1;
+	
+	/**
+	 * Constante que identifica un elemento de tipo reduce
+	 */
+	public static final int REDUCE = 2;
+	
+	/**
+	 * Tipo de elemento (ACEPT, SHIFT o REDUCE)
+	 */
+	private int type;
+	
+	/**
+	 * Valor del elemento
+	 */
+	private int value;
+	
+	/**
+	 * Constructor
+	 * @param kind
+	 */
+	public ActionElement(int type, int value) 
+	{
+		this.type = type;
+		this.value = value;
+	}
+	
+	/**
+	 * Obtiene el tipo de elemento: ACEPT, SHIFT o REDUCE
+	 * @return
+	 */
+	public int getType() 
+	{
+		return type;
+	}
+	
+	/**
+	 * Obtiene el valor (estado a apilar o regla a reducir)
+	 * @return
+	 */
+	public int getValue() 
+	{
+		return value;
+	}
+}
